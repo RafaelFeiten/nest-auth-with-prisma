e89f0b258909b6ecfa4146fe4530f3ae37f46482
Rafael Feiten <rafael.feiten@paipe.co>
2022-11-13 11:28:13 -0300

    implement user findByEmail

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -20,7 +20,7 @@ export class UserService {
     };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
   }
 }
