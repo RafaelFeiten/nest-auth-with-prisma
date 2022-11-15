0191b43268201bde9480cf24d5e7c34d7a6ff98b
Rafael Feiten <rafael.feiten@paipe.co>
2022-11-14 22:47:41 -0300

    local strategy validate user login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
-
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
-  validateUser(email: string, password: string) {
-    throw new Error('Method not implemented.');
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (isPasswordValid) {
+        return user;
+      }
+    }
+    throw new Error('Email or password is incorrect.');
   }
 
   login() {
