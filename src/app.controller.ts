f4869bae8d5d3aaa22a4687f00ebb5d7ce7124b9
Rafael Feiten <rafael.feiten@paipe.co>
2022-11-15 10:41:34 -0300

    implements auth `@CurrentUser`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CurrentUser } from './auth/decorators/current-user.decorator';
+import { IsPublic } from './auth/decorators/is-public.decorator';
+import { User } from './user/entities/user.entity';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @IsPublic()
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('me')
+  getMe(@CurrentUser() user: User) {
+    return user;
   }
 }
