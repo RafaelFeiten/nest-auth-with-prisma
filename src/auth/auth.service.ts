85a900b8e1717d7f1e7d63605e831a3ed700b96c
Rafael Feiten <rafael.feiten@paipe.co>
2022-11-15 10:11:32 -0300

    implement auth login returning jwt token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
+import { UserPayload } from './models/UserPayload';
+import { UserToken } from './models/UserToken';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
@@ -11,13 +18,19 @@ export class AuthService {
     if (user) {
       const isPasswordValid = await bcrypt.compare(password, user.password);
       if (isPasswordValid) {
-        return user;
+        return { ...user, password: undefined };
       }
     }
     throw new Error('Email or password is incorrect.');
   }
 
-  login() {
-    return 'Login';
+  login(user: User): UserToken {
+    const payload: UserPayload = {
+      sub: user.id,
+      email: user.email,
+      name: user.name,
+    };
+    const token = this.jwtService.sign(payload);
+    return { access_token: token };
   }
 }
